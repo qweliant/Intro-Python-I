b0c408fadc15456ddedff875c8eb859008f81d7f
qweliant <qweliantanner@gmail.com>
2020-03-10 14:02:59 -0400

    scoping

--- src/12_scopes.py
+++ src/12_scopes.py
@@ -5,6 +5,7 @@
 x = 12
 
 def change_x():
+    global x
     x = 99
 
 change_x()
@@ -15,18 +16,29 @@ print(x)
 
 # This nested function has a similar problem.
 
-def outer():
+# def outer():
+#     y = 120
+
+#     def inner():
+#         nonlocal y
+#         y = 999
+
+#     inner()
+
+#     # This prints 120. What do we have to change in inner() to get it to print
+#     # 999?
+#     # Note: Google "python nested function scope".
+#     print(y)
+
+
+# outer()
+
+def outer(): #outer function
     y = 120
-
-    def inner():
+    def inner(): #outer function
+        nonlocal y
         y = 999
-
+        
     inner()
-
-    # This prints 120. What do we have to change in inner() to get it to print
-    # 999?
-    # Note: Google "python nested function scope".
-    print(y)
-
-
+    print (y) #prints 2
 outer()
